49952c6b1b784bf5abda9332bc414ac6da60a82b
tomaszlaboj <tomasz.laboj@gmail.com>
2026-04-15 14:06:46 +0100

    add ransom note

--- leet-code/src/arrays/RansomNote.java
+++ leet-code/src/arrays/RansomNote.java
@@ -4,8 +4,13 @@ import java.util.*;
 
 public class RansomNote {
     public static void main(String[] args) {
-        System.out.println(canConstruct("abcc", "abcc"));
-    };
+        System.out.println("canConstruct(\"hello world\", \"hello world\") :" + canConstruct("hello world", "hello world"));
+        System.out.println("canConstruct2(\"hello world\", \"hello world\") :" + canConstruct2("hello world", "hello world"));
+        System.out.println("canConstruct2(\"a\", \"a\") :" + canConstruct2("a", "a"));
+        System.out.println("canConstruct3(\"a\", \"a\") :" + canConstruct3("a", "a"));
+        System.out.println("canConstruct3(\"hello world\", \"asdfasdf\") :" + canConstruct3("hello world", "asdfasdf"));
+        System.out.println("canConstruct3(\"hello world\", \"hello world\") :" + canConstruct3("hello world", "hello world"));
+    }
 
     static public boolean canConstruct(String ransomNote, String magazine) {
         char[] magazineChars = magazine.toCharArray();
@@ -40,8 +45,39 @@ public class RansomNote {
         }
 
         return true;
-    };
+    }
 
+    static public boolean canConstruct2(String ransomNote, String magazine) {
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int charIndex = magazine.indexOf(ransomNote.charAt(i));
+            if ( charIndex >= 0) {
+                magazine = magazine.substring(0, charIndex) + magazine.substring(charIndex + 1);
+            } else {
+                return false;
+            }
+        }
 
+        return true;
+    }
 
+    static public boolean canConstruct3(String ransomNote, String magazine) {
+        Map<Character, Integer> letters = new HashMap<>();
+
+        for (char letter : magazine.toCharArray()) {
+            if (letters.containsKey(letter)) {
+                letters.put(letter, letters.get(letter) + 1);
+            } else {
+                letters.put(letter, 1);
+            }
+        }
+
+        for (char letter : ransomNote.toCharArray()) {
+            if (letters.containsKey(letter) && letters.get(letter) > 0) {
+                letters.put(letter, letters.get(letter) -1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
